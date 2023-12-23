92ecf8dd6844055bcc712126999391be742d96d5
Lesha <maybemuf@gmail.com>
2023-12-23 19:55:05 +0200

    Initial Commit

---
+++ Count Down/Count_DownApp.swift
@@ -0,0 +1,17 @@
+//
+//  Count_DownApp.swift
+//  Count Down
+//
+//  Created by Lesha Melnichenko on 23.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct Count_DownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
